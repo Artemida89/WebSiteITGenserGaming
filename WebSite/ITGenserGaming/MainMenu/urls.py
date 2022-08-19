085b39999663c10da44dbfc09eb2e4f6ae2755e3
Artemida89 <grishinartyomvladimirovich@gmail.com>
2022-08-19 22:52:45 +0300

    Add bg, css style, font size

--- WebSite/ITGenserGaming/MainMenu/urls.py
+++ WebSite/ITGenserGaming/MainMenu/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about_games', views.about_games, name='about_games'),
-    path('about_programming', views.about_programming, name='about_programming'),
-    path('my_work', views.my_work, name='my_work'),
+    path('about_games/', views.about_games, name='about_games'),
+    path('about_programming/', views.about_programming, name='about_programming'),
+    path('my_work/', views.my_work, name='my_work'),
 
-    path('about_games/PC', views.PC, name='about_games/PC'),
-    path('about_games/Android', views.Android, name='about_games/Android'),
-    path('about_games/PlayStation', views.PlayStation, name='about_games/PlayStation'),
-    path('about_games/IOS', views.IOS, name='about_games/IOS'),
+    path('about_games/PC/', views.PC, name='about_games/PC'),
+    path('about_games/Android/', views.Android, name='Android'),
+    path('about_games/PlayStation/', views.PlayStation, name='PlayStation'),
+    path('about_games/IOS/', views.IOS, name='IOS'),
 ]
